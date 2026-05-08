2143e06ad752ce40a41e69d3b440e99732e2df5c
Angus <angus@lgf.homelab.com>
2026-05-08 10:12:24 +0200

    feat: migrate booking notes storage to notes table

--- simple-hotel-crm.php
+++ simple-hotel-crm.php
@@ -11,7 +11,7 @@
 defined( 'ABSPATH' ) || exit;
 
 define( 'SIMPLE_HOTEL_CRM_VERSION', '1.8.9.00' );
-define( 'SIMPLE_HOTEL_CRM_DB_VERSION', '10' );
+define( 'SIMPLE_HOTEL_CRM_DB_VERSION', '11' );
 define( 'SIMPLE_HOTEL_CRM_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
 
 register_activation_hook( __FILE__, 'simple_hotel_crm_activate' );
