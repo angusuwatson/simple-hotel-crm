f75b0d49f8a9de9e45cad568fe30a82095a220fd
Angus <angus@lgf.homelab.com>
2026-05-08 17:22:10 +0200

    chore: simplify booking detail room fields

--- simple-hotel-crm.php
+++ simple-hotel-crm.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: LGF Bookings
  * Description: A simple WordPress-native hotel CRM with calendar and booking management tools
- * Version: 1.8.9.07
+ * Version: 1.8.9.08
  * Update URI: https://github.com/angusuwatson/simple-hotel-crm
  * Author: Angus Watson
  * Text Domain: simple-hotel-crm
@@ -10,7 +10,7 @@
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'SIMPLE_HOTEL_CRM_VERSION', '1.8.9.07' );
+define( 'SIMPLE_HOTEL_CRM_VERSION', '1.8.9.08' );
 define( 'SIMPLE_HOTEL_CRM_DB_VERSION', '12' );
 define( 'SIMPLE_HOTEL_CRM_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
 
